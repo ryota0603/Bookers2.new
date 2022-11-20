930d8f3003f272ebbd6c5d5186c8cdc08008c57c
ryota0603 <114867300+ryota0603@users.noreply.github.com>
2022-11-20 12:37:22 +0000

    flashmessege

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update]
   def index
     @book = Book.new
     @books = Book.all
@@ -11,6 +12,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+    flash[:notice] = "You have created book successfully."  
     redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -39,6 +41,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."   
     redirect_to book_path(@book)
     else
       render :edit
@@ -49,5 +52,11 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 before_action :is_matching_login_user, only: [:edit, :update]
+before_action :correct_user, only: [:edit, :update]
   def index
     @users = User.all
     @book = Book.new
@@ -21,6 +22,7 @@ before_action :is_matching_login_user, only: [:edit, :update]
 
     @user = User.find(params[:id])
     if @user.update(user_params)
+    flash[:notice] = "You have updated user successfully."  
     redirect_to user_path(@user.id)
     else
      
@@ -37,7 +39,11 @@ before_action :is_matching_login_user, only: [:edit, :update]
     user_id = params[:id].to_i
     login_user_id = current_user.id
     if(user_id != login_user_id)
-      redirect_to post_images_path
+      redirect_to user_path(current_user.id)
     end
+  end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to user_path(current_user.id) unless @user == current_user
   end
 end

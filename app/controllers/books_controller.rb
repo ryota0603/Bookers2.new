934ba29723087d791c7701b71e6929b6d0a99012
ryota0603 <114867300+ryota0603@users.noreply.github.com>
2023-01-08 15:23:53 +0000

    deproy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -41,7 +41,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-    flash[:notice] = "You have updated book successfully."   
+    flash[:notice] = "You have updated boo successfully."   
     redirect_to book_path(@book)
     else
       render :edit
